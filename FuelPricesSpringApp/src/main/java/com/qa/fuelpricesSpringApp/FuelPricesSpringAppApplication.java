c18cd16f089c47f46d5d356fd354d3db78aac215
Taliawat <taliawat@gmail.com>
2022-03-24 01:53:59 +0000

    pushing project files

---
+++ FuelPricesSpringApp/src/main/java/com/qa/fuelpricesSpringApp/FuelPricesSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.qa.fuelpricesSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelPricesSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelPricesSpringAppApplication.class, args);
+	}
+
+}
---
+++ FuelPricesSpringApp/src/test/java/com/qa/fuelpricesSpringApp/FuelPricesSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.fuelpricesSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelPricesSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
